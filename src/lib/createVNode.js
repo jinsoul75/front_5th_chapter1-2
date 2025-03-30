4553321b545948536adf70b4b426561d9ca5e6a1
jinsoul75 <wlsthf75@gmail.com>
2025-03-30 10:58:59 +0900

    chore: PR용 커밋

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,9 @@
+const isNotFalsy = (el) => el !== false && el !== null && el !== undefined;
+
 export function createVNode(type, props, ...children) {
-  return {};
+  return {
+    type,
+    props,
+    children: children.flat(Infinity).filter(isNotFalsy),
+  };
 }
