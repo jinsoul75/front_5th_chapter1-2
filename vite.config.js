dbf6b7e4a0c6c7f86f7aeec3fd0c59698c184fc6
jinsoul <wlsthf75@zetalux.co.kr>
2025-04-02 07:49:50 +0900

    chore: 배포 관련 설정

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
 import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
 import { defineConfig } from "vite";
+import path from "path";
 
 export default mergeConfig(
   defineConfig({
+    base: process.env.NODE_ENV === "production" ? "/front_5th_chapter1-1/" : "",
     esbuild: {
       jsxFactory: "createVNode",
+    },
+    build: {
+      outDir: "dist",
+      assetsDir: "assets",
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, "index.html"),
+          hash: path.resolve(__dirname, "index.hash.html"),
+        },
+      },
     },
     optimizeDeps: {
       esbuildOptions: {
