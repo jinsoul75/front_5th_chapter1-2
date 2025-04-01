338132c5f5300a7b34c20f9bf2d8536b1930ad85
jinsoul <wlsthf75@zetalux.co.kr>
2025-04-02 07:38:03 +0900

    fix: 기본과제 완료

--- src/lib/updateElement.js
+++ src/lib/updateElement.js
@@ -3,29 +3,12 @@ import { createElement } from "./createElement.js";
 
 // newProps와 oldProps의 속성을 비교하여 변경된 부분만 반영
 function updateAttributes(target, newProps = {}, oldProps = {}) {
-  // 이전 속성 처리
-  for (const [key, value] of Object.entries(oldProps)) {
-    // 이벤트 처리 (on으로 시작하는 속성)
-    if (key.startsWith("on")) {
-      const eventType = key.toLowerCase().substring(2); // 'onClick' -> 'click'
-
-      if (!newProps[key]) {
-        // 새 속성에 없는 이벤트는 제거
-        addEvent(target, eventType, value);
-      } else {
-        const attributeName = key === "className" ? "class" : key;
-
-        if (newProps[key] !== value) {
-          target.setAttribute(attributeName, newProps[key]);
-        }
-      }
-    }
-  }
-
-  // 새 속성 처리
+  // 다른 돔 갈아끼우기 새 돔 추가
   for (const [key, value] of Object.entries(newProps)) {
     // 이벤트 처리 (on으로 시작하는 속성)
-    if (key.startsWith("on") && typeof value === "function") {
+    if (oldProps[key] === value) return;
+
+    if (key.startsWith("on")) {
       const eventType = key.toLowerCase().substring(2); // 'onClick' -> 'click'
       // 이벤트 리스너 추가 (이전 리스너와 다른 경우)
       if (value !== oldProps[key]) {
@@ -35,6 +18,25 @@ function updateAttributes(target, newProps = {}, oldProps = {}) {
         addEvent(target, eventType, value);
       }
       continue;
+    }
+  }
+
+  // 없어진 돔 처리
+  for (const [key, value] of Object.entries(oldProps)) {
+    // 이벤트 처리 (on으로 시작하는 속성)
+    if (key.startsWith("on")) {
+      const eventType = key.toLowerCase().substring(2); // 'onClick' -> 'click'
+
+      if (!newProps[key]) {
+        // 새 속성에 없는 이벤트는 제거
+        removeEvent(target, eventType, value);
+      } else {
+        const attributeName = key === "className" ? "class" : key;
+
+        if (newProps[key] !== value) {
+          target.setAttribute(attributeName, newProps[key]);
+        }
+      }
     }
   }
 }
@@ -54,7 +56,7 @@ export function updateElement(parentElement, newNode, oldNode, index = 0) {
   if (typeof newNode === "string" && typeof oldNode === "string") {
     if (newNode === oldNode) return;
     return parentElement.replaceChild(
-      document.createTextNode(newNode),
+      createElement(newNode),
       parentElement.childNodes[index],
     );
   }
@@ -70,8 +72,8 @@ export function updateElement(parentElement, newNode, oldNode, index = 0) {
   // old 노드와 new 노드의 태그이름이 같을 경우
   updateAttributes(
     parentElement.childNodes[index],
-    newNode.props,
-    oldNode.props,
+    newNode.props ?? {},
+    oldNode.props ?? {},
   );
 
   // new노드와 old노드의 모든 자식 태그를 순회하며 1~5 내용반복
